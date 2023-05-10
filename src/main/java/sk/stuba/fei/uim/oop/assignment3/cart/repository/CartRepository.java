4dda0b0ab4fe4aa9e3a5330294a88f791fb55049
Vladyslav Nosov <vladorthebest@gmail.com>
2023-05-10 15:33:26 +0200

    Create repository for Cart

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/cart/repository/CartRepository.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/cart/repository/CartRepository.java
@@ -1,2 +1,8 @@
-package sk.stuba.fei.uim.oop.assignment3.cart.repository;public class CartRepository {
+package sk.stuba.fei.uim.oop.assignment3.cart.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import sk.stuba.fei.uim.oop.assignment3.cart.model.Cart;
+
+
+public interface CartRepository extends CrudRepository<Cart, Long> {
 }
